a0dc08e2bd1cbef882e4d84454c123a03b04779d
Gabriela Penaforte <devgabrielapenaforte@gmail.com>
2023-10-13 22:00:55 -0300

    .

---
+++ mf-dev-backend/mf-dev-backend/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using mf_dev_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace mf_dev_backend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Veiculos");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mf-dev-backend/mf-dev-backend/Models/Veiculo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend.Models
+{
+
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigátório informar nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigátório informar a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigátório informar o ano de fabricação!")]
+        [Display(Name ="Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigátório informar o ano do modelo!")]
+        [Display(Name = "Ano do Modelo")]
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
